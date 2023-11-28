a68bfc9dc1edac0385a72c08dcda91da0b57e868
SergiyMilienko <106393416+SergiyMilienko@users.noreply.github.com>
2023-11-28 12:29:25 +0200

    navbar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './style.css'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+        <div className='container'>
+            <div className="nav-row">
+                <a href="/" className="logo">
+                    ТонЧек
+                </a>
+                <ul className="nav-list">
+                    <li className='nav-list__item'>
+                        <a href="/howitworks" className="nav-list__link">Як це працює?</a>
+                    </li>
+                    <li className='nav-list__item'>
+                        <a to="/reviews" className="nav-list__link">Відгуки</a>
+                    </li>
+                    <li className='nav-list__item'>
+                        <a to="/contacts" className="nav-list__link">Контакти</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
